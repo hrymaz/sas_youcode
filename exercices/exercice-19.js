05c01885a3ccf80d9e72ef16f433a336243f9058
RH <rh@HrymazMohamed.local>
2026-09-08 21:29:57 +0100

    code 2

--- exercices/exercice-19.js
+++ exercices/exercice-19.js
@@ -19,3 +19,16 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+let hasAccount = false;
+let isGuest = true;
+let cartValue = 150;
+let isBanned = false;
+
+switch (true) {
+    case !isBanned && (hasAccount || (isGuest && cartValue > 100)):
+        console.log("Commande validée");
+        break;
+
+    default:
+        console.log("Commande refusée");
+}
